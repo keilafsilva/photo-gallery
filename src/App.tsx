7b5f2c2966ec037c9f5061eaca1752b880d27091
Keila <keilafsilva96@gmail.com>
2026-01-21 20:51:03 -0300

    ajustando nome

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const Page = () =>{
   }
 
   return (<div className='mx-2'>
-    <h1 className='text-center text-3xl font-bold my-10'>Fotos Intergaláticas</h1>
+    <h1 className='text-center text-3xl font-bold my-10'>Intergalactic Photos</h1>
     <section className='container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
       {photoList.map(item => (      
         <PhotoItem
