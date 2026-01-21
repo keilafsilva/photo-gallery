6aa0afb842c61d5824a823e8c746ba3182c9890a
Keila <keilafsilva96@gmail.com>
2026-01-21 20:36:36 -0300

    Ajustando imagens

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -8,7 +8,11 @@ const Modal = ({ image, closeModal }: Props) => {
   return (
     <>
       <div onClick={closeModal} className="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center bg-black/90">
-        <img src={`/assets/${image}`} alt="" className="max-w-screem max-h-screen" />
+        <img
+          src={`${import.meta.env.BASE_URL}assets/${image}`}
+          alt=""
+          className="max-w-screen max-h-screen"
+        />
       </div>
    
       <div onClick={closeModal} className="fixed top-5 right-8 w-10 h-10 cursor-pointer text-white text-5xl">
--- src/Components/PhotoItem.tsx
+++ src/Components/PhotoItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 const PhotoItem = ({ photo, onClick }: Props) =>{
   return (
   <div onClick={onClick} className="cursor-pointer hover:opacity-80">
-    <img src={`/assets/${photo.url}`} alt="" className="w-full" />
+    <img src={`${import.meta.env.BASE_URL}assets/${photo.url}`} alt="" className="w-full" />
   </div>
   )
 }
